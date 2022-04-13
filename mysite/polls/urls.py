f4b2f53301af0cdf2b79072ad325e3ec0ba3c17f
Vipin Kumar <vipinthakur3881@gmail.com>
2022-04-14 02:08:48 +0530

    mysite

---
+++ mysite/polls/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+app_name = "polls"
+urlpatterns = [
+    # eg. /polls/
+    path('', views.index, name='index'),
+    # eg. /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # eg. /polls/5/results/
+    path("<int:question_id>/results/", views.result, name="result"),
+    # eg. /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote")
+
+]
